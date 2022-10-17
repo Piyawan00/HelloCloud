b534769173de785150460565fff691f86ab7d67c
Piyawan00 <0893425618.aa@gmail.com>
2022-10-18 02:36:01 +0700

    up

--- HW02/up_rukcom.py
+++ HW02/up_rukcom.py
@@ -5,7 +5,7 @@ app = Flask (__name__)
 
 @app.route('/')
 def index():
-    return render_template('web.html')
+    return render_template('up_web.html')
 
 if __name__ == '__main__':
     app.run(host ='0.0.0.0',port = 80,debug = True)
